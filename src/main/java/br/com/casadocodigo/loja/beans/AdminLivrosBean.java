d4dbace8c6dab05d09d3c8dea2279305de44b662
Lelton Borges <leltondev@gmail.com>
2022-01-01 18:57:58 -0300

    Class autor

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,12 +1,15 @@
 package br.com.casadocodigo.loja.beans;
 
 import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Autor;
 import br.com.casadocodigo.loja.models.Livro;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
 
 @Named
 @RequestScoped
@@ -25,10 +28,17 @@ public class AdminLivrosBean {
         this.livro = livro;
     }
 
+    public List<Autor> getAutores(){
+        return Arrays.asList(
+                new Autor(1, "Lia"),
+                new Autor(2, "Bia"),
+                new Autor(3, "Alex"),
+                new Autor(4, "Bob")
+        );
+    }
+
     @Transactional
     public void salvar(){
-        System.out.println("Livro salvo com Sucesso!");
-        System.out.println(livro);
         livroDao.salvar(livro);
     }
 }
